06bd095897bf389b8b4928478dc9440f5442f1fa
FreemanBoss <bossfreeman9@gmail.com>
2024-10-24 02:25:56 +0000

    done with the tasks

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,72 +1,21 @@
 #!/usr/bin/env python3
-
 """
-Function uses the requests module to obtain the HTML content of a particular
-URL and returns it
+create a web cach
 """
-
-from functools import wraps
-
 import redis
 import requests
-
-store = redis.Redis()
-
-
-def count_url_usage(method):
-    """
-    Decorator function to count URL usage and cache the data
-
-    Function caches the result of the method for a given URL
-
-    Parameters
-        url: URL for which the result should be cached
-
-    Return
-        str: Cached HTML content for the given URL
-    """
-
-    @wraps(method)
-    def wrapper(url):
-        """
-        Wrapper function to cache the result of the method for a given URL
-
-        Parameters
-            url: URL for which the result should be cached
-
-        Return
-            str: Cached HTML content for the given URL
-        """
-
-        cached_key = f"cached:{url}"
-        cached_data = store.get(cached_key)
-
-        if cached_data:
-            return cached_data.decode("utf-8")  # type: ignore
-
-        count_key = f"count:{url}"
-        html = method(url)
-
-        store.incr(count_key)
-        store.set(cached_key, html)
-        store.expire(cached_key, 10)
-        return html
-    return wrapper
+rc = redis.Redis()
+count = 0
 
 
-@count_url_usage
 def get_page(url: str) -> str:
-    """
-    Simply counts the number of times the function was accessed
+    """ get a page and cach value"""
+    rc.set(f"cached:{url}", count)
+    resp = requests.get(url)
+    rc.incr(f"count:{url}")
+    rc.setex(f"cached:{url}", 10, rc.get(f"cached:{url}"))
+    return resp.text
 
-    Retrieves the content of the given URL and returns it as a string
 
-    Parameters
-        url: The URL to retrieve the content from
-
-    Return
-        str: The content of the URL
-    """
-
-    response = requests.get(url)
-    return response.text
+if __name__ == "__main__":
+    get_page('http://slowwly.robertomurray.co.uk')
